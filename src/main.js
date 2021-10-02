eeb32e8dc53bb3dd5b92e7ff280855601de02f75
Don Button <donpublic@digitalgizmo.com>
2021-10-02 12:37:20 -0400

    add apollo composable
    test character query works

--- src/main.js
+++ src/main.js
@@ -1,4 +1,42 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+// For apollo client 
+import { ApolloClient, createHttpLink, InMemoryCache, gql } from '@apollo/client/core'
+// For composable
+import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const httpLink = createHttpLink({
+    uri: 'https://rickandmortyapi.com/graphql',
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
+
+
+// Test access to graphql
+const query = gql`
+    query {
+        characters {
+            results {
+                name
+            }
+        }
+    }
+`
+apolloClient.query({
+    query
+}).then(res => console.log(res))
+
+
+// const app = 
+createApp({
+    setup() {
+        provide(DefaultApolloClient, apolloClient)
+    },
+
+    render: () => h(App),
+}).mount('#app')
