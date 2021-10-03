1fe9baab04e6dffab5c8448d9582990c12a199bf
Don Button <donpublic@digitalgizmo.com>
2021-10-03 14:04:27 -0400

    add working character query

--- src/components/Archive.vue
+++ src/components/Archive.vue
@@ -1,13 +1,36 @@
 <template>
   <h2>{{ title }}  Archive</h2>
   <p>msg:  {{ msg }} </p>
+  <ul v-if="result && result.characters.results">
+    <li v-for="whammy of result.characters.results" :key="whammy.name">
+      {{ whammy.name }}
+    </li>
+  </ul>
 </template>
 
 <script>
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
 export default {
   name: 'Archive',
   props: {
     msg: String
+  },
+  setup () {
+    const { result } = useQuery(gql`
+      query getCharacters {
+        characters {
+          results {
+            name
+          }
+        }
+      }      
+    `)
+
+    return {
+      result,
+    }
   },
   data() {
     return {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp, provide, h } from 'vue'
 // For apollo client 
-import { ApolloClient, createHttpLink, InMemoryCache, gql } from '@apollo/client/core'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+// , gql
 // For composable
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from './App.vue'
@@ -18,18 +19,18 @@ const apolloClient = new ApolloClient({
 
 
 // Test access to graphql
-const query = gql`
-    query {
-        characters {
-            results {
-                name
-            }
-        }
-    }
-`
-apolloClient.query({
-    query
-}).then(res => console.log(res))
+// const query = gql`
+//     query {
+//         characters {
+//             results {
+//                 name
+//             }
+//         }
+//     }
+// `
+// apolloClient.query({
+//     query
+// }).then(res => console.log(res))
 
 
 // const app = 
